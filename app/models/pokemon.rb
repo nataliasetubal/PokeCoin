0357a2c284ba18b06ba678c2725f00450c4c3567
nataliasetubal <natalia.setubal@outlook.com>
2023-01-11 17:37:11 -0300

    adicionando método para atualizar um pokemon quando for feita sua venda

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,13 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
+
+  def sell!
+    raise_pokemon_already_sold_error if sold
+    self.sold = true
+    save
+  end
+
+  def raise_pokemon_already_sold_error(pokemon)
+    PokemonAlreadySoldError.new(pokemon)
+  end
 end
