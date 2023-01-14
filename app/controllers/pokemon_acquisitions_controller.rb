32468a6220d65a76f86b15d6681d42152c4f97d3
nataliasetubal <natalia.setubal@outlook.com>
2023-01-14 20:57:26 -0300

    tratando erros no endpoint de pokemon_acquisitions

--- app/controllers/pokemon_acquisitions_controller.rb
+++ app/controllers/pokemon_acquisitions_controller.rb
@@ -5,8 +5,6 @@ class PokemonAcquisitionsController < AuthenticatedController
     @pokemon_acquisitions = PokemonAcquisition.all
   end
 
-  def show; end
-
   def new
     @pokemon_acquisition = PokemonAcquisition.new
   end
@@ -16,23 +14,22 @@ class PokemonAcquisitionsController < AuthenticatedController
     @pokemon_acquisition = service.execute(pokemon_name: pokemon_name, user_id: current_user.id)
 
     respond_to do |format|
-      format.html { redirect_to pokemon_acquisition_url(@pokemon_purchase), notice: 'Pokemon acquisition was successfully created.' }
+      format.html { redirect_to pokemon_acquisition_url(@pokemon_purchase), notice: 'Nova aquisição de pokémon criada com sucesso.' }
       format.json { render :show, status: :created, location: @pokemon_acquisition }
     end
-  rescue StandardError => e
-    respond_to do |format|
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: @pokemon_acquisition.errors, status: :unprocessable_entity }
-    end
+  rescue PokemonApiException => e
+    redirect_to pokemon_acquisitions_path, notice: e.message
+  rescue StandardError
+    redirect_to pokemon_acquisitions_path, notice: "Ocorreu um erro durante a criação da aquisição do novo pokémon."
   end
 
   private
 
-  def set_pokemon_purchase
+  def set_pokemon_acquisition
     @pokemon_acquisition = PokemonAcquisition.find(params[:id])
   end
 
   def pokemon_name
-    params.require(:pokemon_purchase).require(:name)
+    params.require(:pokemon_acquisition).require(:name)
   end
 end
