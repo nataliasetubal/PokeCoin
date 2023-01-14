f64649b06852185c3a80b0ea77a889c22efb96a3
nataliasetubal <natalia.setubal@outlook.com>
2023-01-14 18:38:56 -0300

    adicionando método que calcula o valor de um pokemon em satoshi

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,11 +1,22 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
+  has_one :pokemon_acquisition
+
+  include ActionView::Helpers::NumberHelper
+
+  SATOSHI_VALUE = 0.00000001
 
   def sell!
     raise_pokemon_already_sold_error if sold
     self.sold = true
     save
   end
+
+  def value_in_satoshis
+    number_to_currency(base_experience * SATOSHI_VALUE, precision: 10, strip_insignificant_zeros: true)
+  end
+
+  private
 
   def raise_pokemon_already_sold_error(pokemon)
     PokemonAlreadySoldError.new(pokemon)
