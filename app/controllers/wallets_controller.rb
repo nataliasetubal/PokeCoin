c7607c6d5ef160d2d2ea00798e02720dde076e6a
nataliasetubal <natalia.setubal@outlook.com>
2023-01-14 18:37:19 -0300

    movendo métodos para a helper de wallet

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,36 +1,8 @@
 class WalletsController < AuthenticatedController
-  before_action :set_wallet, only: %i[ show edit update destroy ]
-
-  # GET /wallets or /wallets.json
   def index
-    @wallet = Wallet.find_by(user_id: current_user.id)
+    @wallet = Wallet.find_by!(user_id: current_user.id)
+  rescue ActiveRecord::RecordNotFound
+    flash.now[:alert] = 'Carteira não encontrada'
+    render :new, status: :unprocessable_entity
   end
-
-  # GET /wallets/1/edit
-  def edit
-  end
-
-  # PATCH/PUT /wallets/1 or /wallets/1.json
-  def update
-    respond_to do |format|
-      if @wallet.update(wallet_params)
-        format.html { redirect_to wallet_url(@wallet), notice: "Wallet was successfully updated." }
-        format.json { render :show, status: :ok, location: @wallet }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @wallet.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_wallet
-      @wallet = Wallet.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def wallet_params
-      params.fetch(:wallet, {})
-    end
 end
--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -1,2 +1,10 @@
 module WalletsHelper
+  def wallet_value_in_usd
+    service = CheckWalletQuotationByUsdService.build
+    service.execute(user_id: current_user.id)
+  end
+
+  def number_of_pokemons
+    Pokemon.where(user_id: current_user.id).count
+  end
 end
