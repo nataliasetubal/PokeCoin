b253a6f2e3ed93e6d00effb4d56eb8a08a503fc4
nataliasetubal <natalia.setubal@outlook.com>
2023-01-15 01:14:33 -0300

    removendo migrações

--- db/migrate/20230114135121_add_precision_to_the_value_column_of_pokemon_sales.rb
+++
@@ -1,5 +0,0 @@
-class AddPrecisionToTheValueColumnOfPokemonSales < ActiveRecord::Migration[7.0]
-  def change
-    change_column(:pokemon_sales, :value, :decimal,  precision: 5, scale: 2)
-  end
-end
--- db/migrate/20230114135541_add_precision_to_the_value_column_of_wallets.rb
+++
@@ -1,5 +0,0 @@
-class AddPrecisionToTheValueColumnOfWallets < ActiveRecord::Migration[7.0]
-  def change
-    change_column(:wallets, :value, :decimal, precision: 5, scale: 2, default: 0)
-  end
-end
