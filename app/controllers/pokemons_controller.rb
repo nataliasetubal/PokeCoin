dd045cef172b1fa5729add3d6060861283cbd573
nataliasetubal <natalia.setubal@outlook.com>
2023-01-14 23:40:16 -0300

    removendo método não utilizado

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,23 +1,20 @@
 class PokemonsController < AuthenticatedController
-  before_action :set_pokemon, only: %i[ show ]
+  before_action :set_pokemon, only: %i[show]
 
   # GET /pokemons or /pokemons.json
   def index
     @pokemons = Pokemon.all
   end
 
-  # GET /pokemons/1 or /pokemons/1.json
-  def show
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pokemon
+    @pokemon = Pokemon.find(params[:id])
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pokemon
-      @pokemon = Pokemon.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def pokemon_params
-      params.require(:pokemon).permit(:name, :base_experience, :sold)
-    end
+  # Only allow a list of trusted parameters through.
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :base_experience, :sold)
+  end
 end
