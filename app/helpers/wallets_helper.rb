4cc38d5300e1cdd896d325951b2050fd72bb9849
nataliasetubal <natalia.setubal@outlook.com>
2023-01-14 23:37:58 -0300

    adicionando método para melhor precisão na exibição do valor da wallet

--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -1,7 +1,13 @@
 module WalletsHelper
+  include ActionView::Helpers::NumberHelper
+
   def wallet_value_in_usd
     service = CheckWalletQuotationByUsdService.build
     service.execute(user_id: current_user.id)
+  end
+
+  def wallet_value_with_precision(value)
+    number_to_currency(value, precision: 10, strip_insignificant_zeros: true)
   end
 
   def number_of_pokemons
