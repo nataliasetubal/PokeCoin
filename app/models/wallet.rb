5d302cf2f89aac3c77d6f85f3c6eedbfe3697b20
nataliasetubal <natalia.setubal@outlook.com>
2023-01-15 01:16:06 -0300

    melhorando método que atualiza o valor da carteira

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -2,11 +2,15 @@ class Wallet < ApplicationRecord
   belongs_to :user
 
   def update_value
-    pokemons_base_experience = Pokemon.where(user_id: user_id).pluck(:base_experience)
+    pokemons_base_experience = Pokemon.where(user_id: user_id, sold: false).pluck(:base_experience)
 
-    pokemons_base_experience.each do |pokemon_base_experience|
-      pokemon_value = ValueConverterToSatoshiService.new.convert(value: pokemon_base_experience)
-      self.value += pokemon_value
+    self.value = 0
+
+    if pokemons_base_experience.present?
+      pokemons_base_experience.each do |pokemon_base_experience|
+        pokemon_value = ValueConverterToSatoshiService.new.convert(value: pokemon_base_experience)
+        self.value += pokemon_value
+      end
     end
 
     save
