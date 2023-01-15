914fff3a4367bcfe004ad26a7dfeba9a9059f40f
nataliasetubal <natalia.setubal@outlook.com>
2023-01-14 23:40:31 -0300

    removendo método de conversão

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,18 +2,10 @@ class Pokemon < ApplicationRecord
   belongs_to :user
   has_one :pokemon_acquisition, ->(id) { where('pokemon_id = ?', id) }
 
-  include ActionView::Helpers::NumberHelper
-
-  SATOSHI_VALUE = 0.00000001
-
   def sell!
     raise_pokemon_already_sold_error if sold
     self.sold = true
     save
-  end
-
-  def value_in_satoshis
-    number_to_currency(base_experience * SATOSHI_VALUE, precision: 10, strip_insignificant_zeros: true)
   end
 
   private
